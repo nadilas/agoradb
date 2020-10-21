dddbaf2e923375ae2005bb88e0b5c92963eeaa68
nadilas <janosveres@me.com>
2020-10-21 21:27:16 +0200

    draft implementation of query methods

--- examples/codefirst/usersdb/database.go
+++ examples/codefirst/usersdb/database.go
@@ -4,7 +4,7 @@ package users
 
 import (
 	"context"
-
+	
 	q "github.com/featme-inc/agoradb/client/query"
 )
 
@@ -55,7 +55,7 @@ func NewDatabase(client *agoradb.Client) UsersDatabase {
 }
 
 func (db *userDatabase) GetUserContext(ctx context.Context, q ...q.Query) GetUserMiddleware {
-	return &getUser{ctx: ctx, c: db.client, q: q...}
+	return &getUserQuery{ctx: ctx, c: db.client, q: q...}
 }
 
 type getUserQuery {
@@ -86,6 +86,134 @@ func (c *getUserQuery) buildMiddlewares() []*GetUserMiddlewareParams {
 func (c *getUserQuery) With(middleware ...q.With) GetUserFinisher {
 	c.m = append(c.m, middleware...)
 	return c
+}
+
+// Returns exactly one User
+// If the query returns more than one rows, this method returns with an error
+func (c *getUserQuery) One() (user User, err error) {
+	resp, err := c.c.GetUsers(c.ctx, &GetUsersRequest{query: c.buildQuery(), middlewares: c.buildMiddlewares()})
+	if err != nil {
+		return User{}, err
+	}
+	
+	resc := len(resp.Users)
+	if resc < 1 {
+		return User{}, q.NoResultsFound
+	} else if resc > 1 {
+		return User{}, q.TooManyResults
+	}
+	
+	return resp.Users[0], nil
+}
+
+// Returns the first n Users
+//
+// n=0 means all entries
+func (c *getUserQuery) First(n int) (users []User, err error) {
+	if n == 0 {
+		return c.All()
+	}
+	
+	resp, err := c.c.GetUsers(c.ctx, &GetUsersRequest{query: c.buildQuery(), middlewares: c.buildMiddlewares()})
+	if err != nil {
+		return User{}, err
+	}
+	
+	resc := len(resp.Users)
+	if resc < 1 {
+		return User{}, q.NoResultsFound
+	}
+	
+	max := n-1
+	if max > resc {
+		max = resc - 1
+	}
+	
+	return resp.Users[0..max], nil
+}
+
+// Returns all Users
+func (c *getUserQuery) All() (users []User, err error) {
+	resp, err := c.c.GetUsers(c.ctx, &GetUsersRequest{query: c.buildQuery(), middlewares: c.buildMiddlewares()})
+	if err != nil {
+		return User{}, err
+	}
+	
+	resc := len(resp.Users)
+	if resc < 1 {
+		return User{}, q.NoResultsFound
+	}
+	
+	return resp.Users, nil
+}
+
+// Returns exactly one User asynchronously
+// If the query returns more than one rows, this method returns with an error
+func (c *getUserQuery) StreamOne(fn func (user User, err error) error) {
+	stream, err := c.c.StreamUsers(c.ctx, &StreamUsersRequest{query: c.buildQuery(), middlewares: c.buildMiddlewares()})
+	if err != nil {
+	  _ = fn(nil, err)
+	  return
+	}
+	c := 0
+	for {
+	    user, err := stream.Recv()
+	    if err == io.EOF {
+			_ = fn(io.EOF)
+	        break
+	    }
+		if err != nil {
+	        log.Println("ERR: %v.StreamUsers(_) = _, %v", c.c, err)
+			break
+	    }
+		c++
+		if c == 2 {
+			_ = fn(User{}, q.TooManyResults)
+			break
+		}
+	    err = fn(user, nil)
+		if err != nil {
+			log.Println("ERR: Cancelled %v.StreamUsers(_) = _, %v", c.c, err)
+			break
+		}
+	}
+}
+
+// Returns the first n Users asynchronously
+func (c *getUserQuery) StreamFirst(n int, fn func (users []User, err error) error) {
+	if n == 0 {
+		c.StreamAll(fn)
+		return
+	}
+	
+	ctx, cancel := context.WithCancel(c.ctx)
+	stream, err := c.c.StreamUsers(ctx, &StreamUsersRequest{query: c.buildQuery(), middlewares: c.buildMiddlewares()})
+	if err != nil {
+	  _ = fn(nil, err)
+	  return
+	}
+	objCounter := 0
+	for {
+	    user, err := stream.Recv()
+	    if err == io.EOF {
+			_ = fn(io.EOF)
+	        break
+	    }
+		if err != nil {
+	        log.Println("ERR: %v.StreamUsers(_) = _, %v", c.c, err)
+			break
+	    }
+		objCounter++
+	    err = fn(user, nil)
+		if err != nil {
+			log.Println("ERR: Cancelled %v.StreamUsers(_) = _, %v", c.c, err)
+			break
+		}
+		if objCounter+1 > n {
+			cancel()
+			break
+		}
+	}
 }
 
 func (c *getUserQuery) StreamAll(fn func (users User, err error) error) {
